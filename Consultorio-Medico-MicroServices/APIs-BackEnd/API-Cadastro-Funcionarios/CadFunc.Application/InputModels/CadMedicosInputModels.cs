c626ded05ba08cdff6ae173e37b7d522a6642411
medinasp <medinasp@gmail.com>
2023-05-20 22:59:16 -0300

    input model

---
+++ Consultorio-Medico-MicroServices/APIs-BackEnd/API-Cadastro-Funcionarios/CadFunc.Application/InputModels/CadMedicosInputModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadFunc.Application.InputModels
+{
+    public class CadMedicosInputModels
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Especialidade { get; set; }
+
+        public CadMedicosService ToEntity()
+            => new CadMedicosService(Nome, CPF, Especialidade);
+    }
+}
