e6bacc3edaf0b913179a39e9bdff0425b66b01d5
medinasp <medinasp@gmail.com>
2023-05-20 22:15:42 -0300

    interfaces repositories

---
+++ Consultorio-Medico-MicroServices/APIs-BackEnd/API-Cadastro-Funcionarios/CadFunc.Domain/InterfacesRepositories/ICadMedicoRepository.cs
@@ -0,0 +1,32 @@
+﻿using CadFunc.Domain.Entities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadFunc.Domain.Interfaces
+{
+    public interface ICadMedicoRepository
+    {
+
+        Task<IEnumerable<CadMedico>> GetAllAsync();
+
+        Task<IEnumerable> GetByIdAsync(int id);
+
+        Task<IEnumerable<CadMedico>> GetByNomeAsync(string nome);
+
+        Task<IEnumerable<CadMedico>> GetByCPFAsync(string cpf);
+
+        Task<IEnumerable<CadMedico>> GetByEspecialidadeAsync(string especialidade);
+
+        Task AddAsync(CadMedico cadMedico);
+
+        Task UpdateAsync(CadMedico cadMedico);
+
+        Task DeleteAsync(int id);
+
+    }
+}
