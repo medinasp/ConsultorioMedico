546aac3dfc89d30d921d48cf965ae95ff94f3744
medinasp <medinasp@gmail.com>
2023-05-21 23:32:33 -0300

    API controller

---
+++ Consultorio-Medico-MicroServices/APIs-BackEnd/API-Cadastro-Funcionarios/CadFunc.API/Controllers/CadMedicosController.cs
@@ -0,0 +1,41 @@
+﻿using CadFunc.Application.InterfacesServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CadFunc.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CadMedicosController : ControllerBase
+    {
+        private readonly ICadMedicosServices _cadMedicosServices;
+
+        public CadMedicosController(ICadMedicosServices cadMedicosServices)
+        {
+            _cadMedicosServices = cadMedicosServices;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CadMedicosInputModels model)
+        {
+            if (ModelState.IsValid)
+            {
+                var id = await _cadMedicosServices.Add(model);
+                return CreatedAtRoute("GetById", new { id }, id);
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        [HttpGet("{id}", Name = "GetById")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var cadMedicosViewModel = await _cadMedicosServices.GetByCode(id);
+
+            if (cadMedicosViewModel == null)
+                return NotFound();
+
+            return Ok(cadMedicosViewModel);
+        }
+    }
+}
