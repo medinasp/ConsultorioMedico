e79452a0bae1f68356390d0b4c044e3c71b7a19b
medinasp <medinasp@gmail.com>
2023-06-11 05:41:47 -0300

    update application

---
+++ Consultorio-Medico-MicroServices/APIs-BackEnd/API-Cadastro-Clientes/CadCli.Application/InputModels/CadClientesInputModel.cs
@@ -0,0 +1,14 @@
+﻿using CadCli.Domain.Entities;
+
+namespace CadCli.Application.InputModels
+{
+    public class CadClientesInputModel
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+
+        public CadCliente ToEntity()
+            => new(Nome, CPF);
+
+    }
+}
---
+++ Consultorio-Medico-MicroServices/APIs-BackEnd/API-Cadastro-Clientes/CadCli.Application/InterfacesServices/ICadClientesService.cs
@@ -0,0 +1,16 @@
+﻿using CadCli.Application.InputModels;
+using CadCli.Application.ViewModels;
+
+namespace CadCli.Application.InterfacesServices
+{
+    public interface ICadClientesService
+    {
+        Task<CadClientesViewModel> Add(CadClientesInputModel model);
+        Task<CadClientesViewModel> GetByCode(string code);
+        Task<IEnumerable<CadClientesViewModel>> GetAll();
+        Task<bool> Update(string id, CadClientesInputModel model);
+        Task<bool> SoftDelete(string id);
+        Task<IEnumerable<CadClientesViewModel>> GetActives();
+        Task<bool> HardDelete(string id);
+    }
+}
