9aa4137be14c664344efeae6e63ac034d9f23563
medinasp <medinasp@gmail.com>
2023-06-12 17:32:10 -0300

    Repositories

---
+++ Consultorio-Medico-MicroServices/APIs-BackEnd/API-Cadastro-Clientes/CadCli.Application/InterfacesRepositories/ICadClientesRepository.cs
@@ -0,0 +1,16 @@
+﻿using CadCli.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadCli.Application.InterfacesRepositories
+{
+    public interface ICadClientesRepository
+    {
+        Task Add(CadCliente cadCliente);
+        //Task<ShippingOrder> GetByCodeAsync(string code);
+        //Task AddAsync(ShippingOrder shippingOrder);
+    }
+}
