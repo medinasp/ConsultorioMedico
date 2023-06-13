2d157793723236bf31782f50c1ee996f896d06a6
medinasp <medinasp@gmail.com>
2023-06-13 20:25:56 -0300

    update domain

---
+++ Consultorio-Medico-MicroServices/APIs-BackEnd/API-Cadastro-Clientes/CadCli.Domain/Entities/CadCliente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadCli.Domain.Entities
+{
+    public class CadCliente : EntityBase
+    {
+        //o método abaixo é exigido para poder rodar as migrations com sucesso
+        private CadCliente() : base("", "")
+        {
+        }
+
+        public CadCliente(string nome, string cpf) : base(nome, cpf)
+        {
+        }
+
+        public void Excluir()
+        {
+            Ativo = false;
+        }
+
+        public void Update(string nome, string cpf)
+        {
+            Nome = nome;
+            CPF = cpf;
+        }
+    }
+}
