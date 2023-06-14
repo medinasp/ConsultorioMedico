a557aa061ffbac2c659cdda03c65e1b625860b1a
medinasp <medinasp@gmail.com>
2023-06-14 11:33:26 -0300

    update Infra

---
+++ Consultorio-Medico-MicroServices/APIs-BackEnd/API-Cadastro-Clientes/CadCli.Infra/Repositories/CadClientesRepository.cs
@@ -0,0 +1,61 @@
+﻿using CadCli.Domain.Entities;
+using CadCli.Infra.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CadCli.Infra.Repositories
+{
+    public class CadClientesRepository : ICadClientesRepository
+    {
+        private readonly ContextBase _context;
+
+        public CadClientesRepository(ContextBase context)
+        {
+            _context = context;
+        }
+
+        public async Task Add(CadCliente cadCliente)
+        {
+            await _context.CadClientes.AddAsync(cadCliente);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<CadCliente>> GetActives()
+        {
+            return await _context.CadClientes.Where(c => c.Ativo).ToListAsync();
+        }
+
+        public async Task<IEnumerable<CadCliente>> GetAll()
+        {
+            return await _context.CadClientes.ToListAsync();
+        }
+
+        public async Task<CadCliente> GetByCode(string code)
+        {
+            if (!Guid.TryParse(code, out var guid))
+                return null;
+
+            var cadCliente = await _context.CadClientes.FindAsync(guid);
+
+            return cadCliente;
+        }
+
+        public async Task HardDelete(CadCliente cadCliente)
+        {
+            _context.CadClientes.Remove(cadCliente);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task<bool> SoftDelete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Update(CadCliente cadCliente)
+        {
+            _context.CadClientes.Update(cadCliente);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
+
---
+++ Consultorio-Medico-MicroServices/APIs-BackEnd/API-Cadastro-Clientes/CadCli.Infra/Repositories/ICadClientesRepository.cs
@@ -0,0 +1,15 @@
+﻿using CadCli.Domain.Entities;
+
+namespace CadCli.Infra.Repositories
+{
+    public interface ICadClientesRepository
+    {
+        Task Add(CadCliente cadCliente);
+        Task<CadCliente> GetByCode(string code);
+        Task<IEnumerable<CadCliente>> GetAll();
+        Task Update(CadCliente cadCliente);
+        Task<bool> SoftDelete(string id);
+        Task<IEnumerable<CadCliente>> GetActives();
+        Task HardDelete(CadCliente cadCliente);
+    }
+}
