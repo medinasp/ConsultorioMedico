9f47386091881fe80b37c818ae314cffccc0e411
medinasp <medinasp@gmail.com>
2023-06-14 11:32:09 -0300

    update Infra

---
+++ Consultorio-Medico-MicroServices/APIs-BackEnd/API-Cadastro-Clientes/CadCli.Infra/InfraStructureModule.cs
@@ -0,0 +1,23 @@
+﻿using CadCli.Infra.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CadCli.Infra
+{
+    public static class InfraStructureModule
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services
+                .AddInfraRepositories();
+
+            return services;
+        }
+
+        private static IServiceCollection AddInfraRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<ICadClientesRepository, CadClientesRepository>();
+
+            return services;
+        }
+    }
+}
