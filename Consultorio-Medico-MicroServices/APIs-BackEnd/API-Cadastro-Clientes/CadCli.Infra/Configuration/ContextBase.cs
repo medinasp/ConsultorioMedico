85beaa6b0932e214b505c56de8a94bba95d03a88
medinasp <medinasp@gmail.com>
2023-06-14 11:30:53 -0300

    update Infra

---
+++ Consultorio-Medico-MicroServices/APIs-BackEnd/API-Cadastro-Clientes/CadCli.Infra/Configuration/ContextBase.cs
@@ -0,0 +1,29 @@
+﻿using CadCli.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CadCli.Infra.Configuration
+{
+    public class ContextBase : DbContext
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+        }
+
+        public DbSet<CadCliente> CadClientes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ContextBase).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
