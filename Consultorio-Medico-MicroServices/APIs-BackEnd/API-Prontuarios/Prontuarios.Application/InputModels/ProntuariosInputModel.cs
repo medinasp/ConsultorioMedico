c676aa7e7a1b11a3d3e71104b1b7baf3f2e5c078
medinasp <medinasp@gmail.com>
2023-06-19 18:37:54 -0300

    update Interface

--- Consultorio-Medico-MicroServices/APIs-BackEnd/API-Prontuarios/Prontuarios.Application/InputModels/ProntuariosInputModel.cs
+++ Consultorio-Medico-MicroServices/APIs-BackEnd/API-Prontuarios/Prontuarios.Application/InputModels/ProntuariosInputModel.cs
@@ -1,21 +1,28 @@
 ﻿using Prontuarios.Domain.Entities;
-using Prontuarios.Domain.Entities.External;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Prontuarios.Application.InputModels
 {
+    //public class ProntuariosInputModel
+    //{
+    //    public CadMedicos Medico { get; set; }
+    //    public CadClientes Cliente { get; set; }
+    //    public string TextoProntuario { get; set; }
+
+    //    public Prontuario ToEntity()
+    //        => new(Medico, Cliente, TextoProntuario);
+
+    //}
+
     public class ProntuariosInputModel
     {
-        public CadMedicos Medico { get; set; }
-        public CadClientes Cliente { get; set; }
         public string TextoProntuario { get; set; }
+        public string MedicoId { get; set; }
+        public string PacienteId { get; set; }
 
         public Prontuario ToEntity()
-            => new(Medico, Cliente, TextoProntuario);
-
+        {
+            return new Prontuario(MedicoId, PacienteId, TextoProntuario);
+        }
     }
+
 }
