0d000149b1ff534a1bce691efd3b6d01f89b9fda
medinasp <medinasp@gmail.com>
2023-06-19 18:40:08 -0300

    update service

--- Consultorio-Medico-MicroServices/APIs-BackEnd/API-Prontuarios/Prontuarios.Application/Services/ProntuariosService.cs
+++ Consultorio-Medico-MicroServices/APIs-BackEnd/API-Prontuarios/Prontuarios.Application/Services/ProntuariosService.cs
@@ -1,9 +1,7 @@
 ﻿using Prontuarios.Application.InputModels;
 using Prontuarios.Application.InterfaceServices;
 using Prontuarios.Application.ViewModels;
-using Prontuarios.Domain.Entities.External;
 using Prontuarios.Domain.Entities;
-using Newtonsoft.Json;
 using Prontuarios.Infra.Repositories;
 
 namespace Prontuarios.Application.Services
@@ -19,101 +17,172 @@ namespace Prontuarios.Application.Services
             _prontuariosRepository = prontuariosRepository;
         }
 
-        public async Task<ProntuariosViewModel> CriarProntuarioPorId(ProntuariosInputModel model)
+        public async Task CriarProntuario2(ProntuariosInputModel model)
         {
             // Obter dados do médico da API externa
-            var medicoResponse = await _httpClient.GetAsync($"https://localhost:7176/api/CadMedicos/{model.Medico.Id}");
+            var medicoResponse = await _httpClient.GetAsync($"https://localhost:7176/api/CadMedicos/{model.MedicoId}");
             if (!medicoResponse.IsSuccessStatusCode)
             {
                 // Tratar erro ao obter os dados do médico
-                return null;
+                return;
             }
 
             var medicoContent = await medicoResponse.Content.ReadAsStringAsync();
-            var medico = JsonConvert.DeserializeObject<CadMedicos>(medicoContent);
+            var medicoId = medicoContent; // Armazena o ID do médico como string
 
-            // Obter dados do cliente da API externa
-            var clienteResponse = await _httpClient.GetAsync($"https://localhost:7099/api/CadClientes/{model.Cliente.Id}");
-            if (!clienteResponse.IsSuccessStatusCode)
+            // Obter dados do paciente da API externa
+            var pacienteResponse = await _httpClient.GetAsync($"https://localhost:7099/api/CadClientes/{model.PacienteId}");
+            if (!pacienteResponse.IsSuccessStatusCode)
             {
-                // Tratar erro ao obter os dados do cliente
-                return null;
+                // Tratar erro ao obter os dados do paciente
+                return;
             }
 
-            var clienteContent = await clienteResponse.Content.ReadAsStringAsync();
-            var cliente = JsonConvert.DeserializeObject<CadClientes>(clienteContent);
+            var pacienteContent = await pacienteResponse.Content.ReadAsStringAsync();
+            var pacienteId = pacienteContent; // Armazena o ID do paciente como string
 
-            // Criar o prontuário
-            var prontuario = new Prontuario(medico, cliente, model.TextoProntuario);
+            // Criar o prontuário com os dados obtidos
+            var prontuario = new Prontuario(medicoId, pacienteId, model.TextoProntuario);
 
             // Persistir o prontuário no banco de dados
-            await _prontuariosRepository.Add(prontuario);
-
-            // Mapear o prontuário para o ViewModel
-            var viewModel = new ProntuariosViewModel
-            {
-                Id = prontuario.Id,
-                IdPaciente = Guid.Parse(prontuario.Cliente.Id),
-                NomePaciente = prontuario.Cliente.Nome,
-                CPFPaciente = prontuario.Cliente.CPF,
-                DataCriacaoPaciente = prontuario.Cliente.DataCriacao,
-                IdMedico = Guid.Parse(prontuario.Medico.Id),
-                NomeMedico = prontuario.Medico.Nome,
-                CPFMedico = prontuario.Medico.CPF,
-                EspecialidadeMedico = prontuario.Medico.Especialidade,
-                TextoProntuario = prontuario.TextoProntuario
-            };
-
-            return viewModel;
+            _prontuariosRepository.Add(prontuario);
         }
 
-        public async Task<ProntuariosViewModel> CriarProntuarioPorNome(ProntuariosInputModel model)
+
+
+        public Task<ProntuariosViewModel> CriarProntuarioPorId(ProntuariosInputModel model)
         {
-            // Obter dados do médico da API externa
-            var medicoResponse = await _httpClient.GetAsync($"https://localhost:7176/api/CadMedicos/{model.Medico.Nome}");
-            if (!medicoResponse.IsSuccessStatusCode)
-            {
-                // Tratar erro ao obter os dados do médico
-                return null;
-            }
-
-            var medicoContent = await medicoResponse.Content.ReadAsStringAsync();
-            var medico = JsonConvert.DeserializeObject<CadMedicos>(medicoContent);
-
-            // Obter dados do cliente da API externa
-            var clienteResponse = await _httpClient.GetAsync($"https://localhost:7099/api/CadClientes/{model.Cliente.Nome}");
-            if (!clienteResponse.IsSuccessStatusCode)
-            {
-                // Tratar erro ao obter os dados do cliente
-                return null;
-            }
-
-            var clienteContent = await clienteResponse.Content.ReadAsStringAsync();
-            var cliente = JsonConvert.DeserializeObject<CadClientes>(clienteContent);
-
-            // Criar o prontuário
-            var prontuario = new Prontuario(medico, cliente, model.TextoProntuario);
-
-            // Persistir o prontuário no banco de dados
-            await _prontuariosRepository.Add(prontuario);
-
-            // Mapear o prontuário para o ViewModel
-            var viewModel = new ProntuariosViewModel
-            {
-                Id = prontuario.Id,
-                IdPaciente = Guid.Parse(prontuario.Cliente.Id),
-                NomePaciente = prontuario.Cliente.Nome,
-                CPFPaciente = prontuario.Cliente.CPF,
-                DataCriacaoPaciente = prontuario.Cliente.DataCriacao,
-                IdMedico = Guid.Parse(prontuario.Medico.Id),
-                NomeMedico = prontuario.Medico.Nome,
-                CPFMedico = prontuario.Medico.CPF,
-                EspecialidadeMedico = prontuario.Medico.Especialidade,
-                TextoProntuario = prontuario.TextoProntuario
-            };
-
-            return viewModel;
+            throw new NotImplementedException();
         }
+
+        //public async Task CriarProntuario(ProntuariosInputModel model)
+        //{
+        //    // Obter dados do médico da API externa
+        //    var medicoResponse = await _httpClient.GetAsync($"https://localhost:7176/api/CadMedicos/{model.MedicoId}");
+        //    if (!medicoResponse.IsSuccessStatusCode)
+        //    {
+        //        // Tratar erro ao obter os dados do médico
+        //        return;
+        //    }
+
+        //    var medicoContent = await medicoResponse.Content.ReadAsStringAsync();
+        //    var medico = JsonConvert.DeserializeObject<CadMedicos>(medicoContent);
+
+        //    // Obter dados do cliente da API externa
+        //    var clienteResponse = await _httpClient.GetAsync($"https://localhost:7099/api/CadClientes/{model.PacienteId}");
+        //    if (!clienteResponse.IsSuccessStatusCode)
+        //    {
+        //        // Tratar erro ao obter os dados do cliente
+        //        return;
+        //    }
+
+        //    var clienteContent = await clienteResponse.Content.ReadAsStringAsync();
+        //    var cliente = JsonConvert.DeserializeObject<CadClientes>(clienteContent);
+
+        //    // Criar o prontuário apenas com os dados necessários
+        //    var prontuario = new Prontuario(model.MedicoId, model.PacienteId, model.TextoProntuario);
+
+        //    // Persistir o prontuário no banco de dados
+        //    _prontuariosRepository.Add(prontuario);
+        //}
+
+        //public async Task<ProntuariosViewModel> CriarProntuarioPorId(ProntuariosInputModel model)
+        //{
+        //    // Obter dados do médico da API externa
+        //    var medicoResponse = await _httpClient.GetAsync($"https://localhost:7176/api/CadMedicos/{model.Medico.Id}");
+        //    if (!medicoResponse.IsSuccessStatusCode)
+        //    {
+        //        // Tratar erro ao obter os dados do médico
+        //        return null;
+        //    }
+
+        //    var medicoContent = await medicoResponse.Content.ReadAsStringAsync();
+        //    var medico = JsonConvert.DeserializeObject<CadMedicos>(medicoContent);
+
+        //    // Obter dados do cliente da API externa
+        //    var clienteResponse = await _httpClient.GetAsync($"https://localhost:7099/api/CadClientes/{model.Cliente.Id}");
+        //    if (!clienteResponse.IsSuccessStatusCode)
+        //    {
+        //        // Tratar erro ao obter os dados do cliente
+        //        return null;
+        //    }
+
+        //    var clienteContent = await clienteResponse.Content.ReadAsStringAsync();
+        //    var cliente = JsonConvert.DeserializeObject<CadClientes>(clienteContent);
+
+        //    // Criar o prontuário
+        //    var prontuario = new Prontuario(medico, cliente, model.TextoProntuario);
+        //    prontuario.Medico = medico;
+        //    prontuario.Cliente = cliente;
+
+        //    // Persistir o prontuário no banco de dados
+        //    await _prontuariosRepository.Add(prontuario);
+
+        //    // Mapear o prontuário para o ViewModel
+        //    var viewModel = new ProntuariosViewModel
+        //    {
+        //        Id = prontuario.Id,
+        //        IdPaciente = Guid.Parse(prontuario.Cliente.Id),
+        //        NomePaciente = prontuario.Cliente.Nome,
+        //        CPFPaciente = prontuario.Cliente.CPF,
+        //        DataCriacaoPaciente = prontuario.Cliente.DataCriacao,
+        //        IdMedico = Guid.Parse(prontuario.Medico.Id),
+        //        NomeMedico = prontuario.Medico.Nome,
+        //        CPFMedico = prontuario.Medico.CPF,
+        //        EspecialidadeMedico = prontuario.Medico.Especialidade,
+        //        TextoProntuario = prontuario.TextoProntuario
+        //    };
+
+        //    return viewModel;
+        //}
+
+        //public async Task<ProntuariosViewModel> CriarProntuarioPorNome(ProntuariosInputModel model)
+        //{
+        //    // Obter dados do médico da API externa
+        //    var medicoResponse = await _httpClient.GetAsync($"https://localhost:7176/api/CadMedicos/{model.Medico.Nome}");
+        //    if (!medicoResponse.IsSuccessStatusCode)
+        //    {
+        //        // Tratar erro ao obter os dados do médico
+        //        return null;
+        //    }
+
+        //    var medicoContent = await medicoResponse.Content.ReadAsStringAsync();
+        //    var medico = JsonConvert.DeserializeObject<CadMedicos>(medicoContent);
+
+        //    // Obter dados do cliente da API externa
+        //    var clienteResponse = await _httpClient.GetAsync($"https://localhost:7099/api/CadClientes/{model.Cliente.Nome}");
+        //    if (!clienteResponse.IsSuccessStatusCode)
+        //    {
+        //        // Tratar erro ao obter os dados do cliente
+        //        return null;
+        //    }
+
+        //    var clienteContent = await clienteResponse.Content.ReadAsStringAsync();
+        //    var cliente = JsonConvert.DeserializeObject<CadClientes>(clienteContent);
+
+        //    // Criar o prontuário
+        //    var prontuario = new Prontuario(medico, cliente, model.TextoProntuario);
+
+        //    // Persistir o prontuário no banco de dados
+        //    await _prontuariosRepository.Add(prontuario);
+
+        //    // Mapear o prontuário para o ViewModel
+        //    var viewModel = new ProntuariosViewModel
+        //    {
+        //        Id = prontuario.Id,
+        //        IdPaciente = Guid.Parse(prontuario.Cliente.Id),
+        //        NomePaciente = prontuario.Cliente.Nome,
+        //        CPFPaciente = prontuario.Cliente.CPF,
+        //        DataCriacaoPaciente = prontuario.Cliente.DataCriacao,
+        //        IdMedico = Guid.Parse(prontuario.Medico.Id),
+        //        NomeMedico = prontuario.Medico.Nome,
+        //        CPFMedico = prontuario.Medico.CPF,
+        //        EspecialidadeMedico = prontuario.Medico.Especialidade,
+        //        TextoProntuario = prontuario.TextoProntuario
+        //    };
+
+        //    return viewModel;
+        //}
 
 
     }
