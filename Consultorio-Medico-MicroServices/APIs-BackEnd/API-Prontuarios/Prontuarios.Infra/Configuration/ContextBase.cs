9c06f3a0521b886c9a06ccb5e45e8017504a8807
medinasp <medinasp@gmail.com>
2023-06-20 08:13:47 -0300

    update context

--- Consultorio-Medico-MicroServices/APIs-BackEnd/API-Prontuarios/Prontuarios.Infra/Configuration/ContextBase.cs
+++ Consultorio-Medico-MicroServices/APIs-BackEnd/API-Prontuarios/Prontuarios.Infra/Configuration/ContextBase.cs
@@ -1,11 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Prontuarios.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Emit;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Prontuarios.Infra.Configuration
 {
