9f88f682dcba0b8dcef32f466abb699a26b96018
medinasp <medinasp@gmail.com>
2023-06-21 18:37:16 -0300

    update Prontuario.cs

--- Consultorio-Medico-MicroServices/APIs-BackEnd/API-Prontuarios/Prontuarios.Domain/Entities/Prontuario.cs
+++ Consultorio-Medico-MicroServices/APIs-BackEnd/API-Prontuarios/Prontuarios.Domain/Entities/Prontuario.cs
@@ -3,17 +3,23 @@
     public class Prontuario : EntityBase
     {
         public string MedicoId { get; set; }
+        public string MedicoNome { get; set; }
+        public string MedicoEspecialidade { get; set; }
         public string PacienteId { get; set; }
+        public string PacienteNome { get; set; }
         public string TextoProntuario { get; set; }
 
         private Prontuario()
         {
         }
 
-         public Prontuario(string medicoId, string pacienteId, string textoProntuario)
+         public Prontuario(string medicoId, string medicoNome, string medicoEspecialidade, string pacienteId, string pacienteNome, string textoProntuario)
         {
             MedicoId = medicoId;
+            MedicoNome = medicoNome;
+            MedicoEspecialidade = medicoEspecialidade;
             PacienteId = pacienteId;
+            PacienteNome = pacienteNome;
             TextoProntuario = textoProntuario;
         }
 
